e9a8017ac15047e72dc6982afadcfeb063b92409
eldemoniodice <joan_19-02@hotmail.com>
2017-10-02 23:24:52 -0500

    Add files via upload

---
+++ Pregunta_2.py
@@ -0,0 +1,171 @@
+import sys
+import sqlite3
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+class Entrada:
+    def __init__(self, pelicula_id, funcion, cantidad):
+        self.pelicula_id = pelicula_id
+        self.funcion = funcion
+        self.cantidad = cantidad
+
+class Pelicula:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+        
+#Prototype
+class PeliculaPrototype:
+    def __clone__(self,id,nombre):
+        return copy.deepcopy(self)
+
+class CinePlaneta:
+    def __init__(self):
+        peliculaIT = Pelicula(1, 'IT')
+        peliculaHF = Pelicula(2, 'La Hora Final')
+        #Prototype
+        peliculaD = PeliculaPrototype(3, 'Desparecido')
+        peliculaDeep = PeliculaPrototype(4, 'Deep El Pulpo')
+
+        peliculaIT.funciones = ['19:00', '20.30', '22:00']
+        peliculaHF.funciones = ['21:00']
+        peliculaD.funciones = ['20:00', '23:00']
+        peliculaDeep.funciones = ['16:00']
+
+        self.lista_peliculas = [peliculaIT, peliculaHF, peliculaD, peliculaDeep]
+        self.entradas = []
+
+    def listar_peliculas(self):
+        return self.lista_peliculas
+
+    def listar_funciones(self, pelicula_id):
+        return self.lista_peliculas[int(pelicula_id) - 1].funciones
+
+    def guardar_entrada(self, id_pelicula_elegida, funcion_elegida, cantidad):
+        self.entradas.append(Entrada(id_pelicula_elegida, funcion_elegida, cantidad))
+        return len(self.entradas)
+
+
+
+class CineStark:
+    def __init__(self):
+        #Prototype
+        peliculaD = peliculaD.__clone__()
+        peliculaDeep = peliculaDeep.__clone__()
+
+        peliculaD.funciones = ['21:00', '23:00']
+        peliculaDeep.funciones = ['16:00', '20:00']
+
+        self.lista_peliculas = [peliculaD, peliculaDeep]
+        self.entradas = []
+
+
+    def listar_peliculas(self):
+        return self.lista_peliculas
+
+    def listar_funciones(self, pelicula_id):
+        return self.lista_peliculas[int(pelicula_id) - 1].funciones
+
+    def guardar_entrada(self, id_pelicula_elegida, funcion_elegida, cantidad):
+        self.entradas.append(Entrada(id_pelicula_elegida, funcion_elegida, cantidad))
+        return len(self.entradas)
+
+#Clase Factory
+class CarteleraFactory:
+    def obtener_cartelera(self,cine):
+        if cine == '1':
+            return CinePlaneta()
+        elif cine == '2':
+            return CineStark()
+        else:
+            return None
+
+#Decorator
+def f_decoradora(func_a_decorar):
+    def f_wrapper(dicc):
+        return func_a_decorar(dicc).upper()
+    return f_wrapper
+
+def formatear_xml(diccionario):
+    cad = '<data>\n'
+    for key, value in diccionario.items():
+        cad  = cad + '\t<%s>%s</%s>\n' % (key, value, key)
+    cad = cad + '</data>\n'
+    return cad
+
+
+def main():
+    terminado = False;
+    while not terminado:
+        print('Ingrese la opción que desea realizar')
+        print('(1) Listar cines')
+        print('(2) Listar cartelera')
+        print('(3) Comprar entrada')
+        print('(0) Salir')
+        opcion = input('Opción: ')
+
+        if opcion == '1':
+            print('********************')
+            print('Lista de cines')
+            print('1: CinePlaneta')
+            print('2: CineStark')
+            print('********************')
+
+        elif opcion == '2':
+            print('********************')
+            print('Lista de cines')
+            print('1: CinePlaneta')
+            print('2: CineStark')
+            print('********************')
+
+            cine = input('Primero elija un cine:')
+            if cine == '1':
+                # CinePlaneta
+                cine = CinePlaneta()
+            elif cine == '2':
+                cine = CineStark()
+
+            factory = CarteleraFactory() # instanciamos el factory
+            cartelera = factory.obtener_cartelera(sys.argv[1])
+            cartelera.listar_peliculas()
+            
+            print('********************')
+            for pelicula in peliculas:
+                print("{}. {}".formatear_xml(pelicula.id, pelicula.nombre))
+            print('********************')
+
+        elif opcion == '3':
+            print('********************')
+            print('COMPRAR ENTRADA')
+            print('Lista de cines')
+            print('1: CinePlaneta')
+            print('2: CineStark')
+            print('********************')
+            cine = input('Primero elija un cine:')
+            if cine == '1':
+                # CinePlaneta
+                cine = CinePlaneta()
+            elif cine == '2':
+                cine = CineStark()
+
+            peliculas = cine.listar_peliculas()
+            for pelicula in peliculas:
+                print("{}. {}".formatear_xml(pelicula.id, pelicula.nombre))
+            pelicula_elegida = input('Elija pelicula:')
+            #pelicula = obtener_pelicula(id_pelicula)
+            print('Ahora elija la función (debe ingresar el formato hh:mm): ')
+            for funcion in cine.listar_funciones(pelicula_elegida):
+                print('Función: {}'.f_decorada(funcion))
+            funcion_elegida = input('Funcion:')
+            cantidad_entradas = input('Ingrese cantidad de entradas: ')
+            codigo_entrada = cine.guardar_entrada(pelicula_elegida, funcion_elegida, cantidad_entradas)
+            print('Se realizó la compra de la entrada. Código es {}'.f_decorada(codigo_entrada))
+        elif opcion == '0':
+            terminado = True
+        else:
+            print(opcion)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md.txt
@@ -0,0 +1,12 @@
+NOMBRE: JOAN PINTO ALBORNOZ
+CODIGO: 20112181
+USUARIO GITHUB: eldemoniodice
+
+La pregunta 1 se encuentra en un documentos txt llamado Pregunta_1.txt
+
+Para la pregunta 2, aplique los siguientes patrones:
+-Factory: Queria obtener la cartelera de una manera mas eficiente
+-Prototype: Ya que ambos cines coincidian en dos peliculas
+-Decorator: Para darle un aspecto más agradable a los prints
+
+La pregunta 3 se encuentra en el archivo py llamado Pregunta_2.py
